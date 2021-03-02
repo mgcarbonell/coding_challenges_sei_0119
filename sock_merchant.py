1244dff56f10ac5fb6428df9679aaa68b617ec1b
mgcarbonell <mgcarbonell@gmail.com>
2021-03-02 09:39:50 -0800

    Formatting

--- sock_merchant.py
+++ sock_merchant.py
@@ -1,16 +1,18 @@
-# There is a large pile of socks that must be paired by
-# color. Given an array of integers representing the
-# color of each sock, determine how many pairs of socks
-# with matching colors there are.
-# 
-# Example:
-# n = 7
-# ar = [1,2,1,2,1,3,2]
-# 
-# There is one pair of color 1, and one of color 3.
-# There are three odd socks left, one of each color.
-# The numbers of pairs is 2.# 
-# Complete the sockMerchant function:
-# sockMerchant has the following parameter(s):
-# int n: the number of socks in the pile
-# int ar[n]: the colors of each sock.
+##############################################################
+# There is a large pile of socks that must be paired by      #
+# color. Given an array of integers representing the         #
+# color of each sock, determine how many pairs of socks      #
+# with matching colors there are.                            #
+#                                                            #
+# Example:                                                   #
+# n = 7                                                      #
+# ar = [1,2,1,2,1,3,2]                                       #
+#                                                            #
+# There is one pair of color 1, and one of color 3.          #
+# There are three odd socks left, one of each color.         #
+# The numbers of pairs is 2.#                                #
+# Complete the sockMerchant function:                        #
+# sockMerchant has the following parameter(s):               #
+# int n: the number of socks in the pile                     #
+# int ar[n]: the colors of each sock.                        #
+##############################################################
