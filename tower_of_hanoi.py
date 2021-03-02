7dedb0efc2a8789eb302eee2e4e97e6dc887db69
mgcarbonell <mgcarbonell@gmail.com>
2021-03-02 09:35:03 -0800

    Adds intructions for tower of hanoi

---
+++ tower_of_hanoi.py
@@ -0,0 +1,13 @@
+#################################################################
+# There is a room in a temple in Hanoi with three time worn     #
+# poles. According to an ancient Buddhist prophecy, a Buddhist  #
+# monk must move 64 golden discs from one pole to the next.     #
+# When the monk finishes, the world will end.                   #
+                                                                #
+# Here are the rules:                                           #
+# 1. Only one disk can be moved at one time.                    #
+# 2. Each move consists of taking the upper disk from one       #
+#     of the stacks and placing it on top of another stack      #
+#     or on an empty pole.                                      # 
+# 3. No larger disk may be placed on top of a smaller disk.     #
+#################################################################
