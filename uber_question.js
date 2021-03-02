ee55c4f2adbd18e406ef71a37748a86d14385344
mgcarbonell <mgcarbonell@gmail.com>
2021-03-02 10:22:27 -0800

    Adds instructions to uber question

---
+++ uber_question.js
@@ -0,0 +1,7 @@
+/******************************************************************************
+*  For this task, you will be given the elements of a perfect binary tree of  * 
+*  characters, sorted within a simple tree data structure.                    *
+*                                                                             *
+*  Your goal is to write a function that starts at the root of the tree and   *
+*  returns a counter clockwise traversal of the nodes at the edge of tree.    *
+******************************************************************************/
